c4cf6de92c57511aa282ab0828400047bd1101d2
ullipr <103921212+ullipr@users.noreply.github.com>
2022-04-18 12:12:28 +0500

    Create sabaqliq-5.py

---
+++ sabaqliq-5.py
@@ -0,0 +1,22 @@
+#Sabalıq-5 Python Sabaqları Tekst benen islew
+
+At = "Azamat"
+Familya = "Perdebaev"
+ati_familyasi = f"{ati} {familyasi}"
+ati_familyasi = ati_familyasi.upper()
+print(ati_familyasi)
+print(ati_familyadi.lower())
+print(ati_familyasi.capitalize())
+print(ati_familyasi.title())
+
+
+#Teksttıń shep yaki oń tàrefindeģı bos órınlardı alip taslaw
+
+
+miyweler = " Alma  "
+print(miywrler.lstrip(), + "","Júda jaqsı kóremen")
+print(miywrler.rstrip(), + "","Júda jaqsı kóremen")
+print(miywrler.strip(), + "","Júda jaqsı kóremen")
+
+atim = input("Atıńız kim")
+print("Assalawma àleykum," + atim)
